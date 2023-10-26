bfd40462808c2f87239b23cc94fb142159477460
Jason <pjb6510@gmail.com>
2023-10-27 04:48:28 +0900

    add: 10-3.module-importing_name

--- 10-1.module-communicator/src/lib.rs
+++ 10-1.module-communicator/src/lib.rs
@@ -7,11 +7,10 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::client;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        client::connect();
     }
 }
--- 10-3.module-importing_name/src/main.rs
+++ 10-3.module-importing_name/src/main.rs
@@ -1,3 +1,38 @@
+#![allow(unused)]
+
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {
+                println!("hello modules!");
+            }
+        }
+    }
+}
+
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+enum MouseEvent {
+    Down,
+    Up,
+    Enter,
+    Leave,
+}
+
+use a::series::of;
+use MouseEvent::*;
+use TrafficLight::{Red, Yellow};
+
 fn main() {
-    println!("Hello, world!");
+    of::nested_modules();
+
+    let red = Red;
+    let yellow = Yellow;
+    let green = TrafficLight::Green;
+
+    let mousedown = Down;
 }
